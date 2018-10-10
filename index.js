06286e7ba6ce8dbf6d1e7bded85e199290fbf0bb
Donna Cadiente <djanecad@hotmail.com>
2018-10-10 17:47:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var dodger = document.getElementById('dodger')
 function moveDodgerRight() {
-  var leftNumbers = dodger.style.right.replace('px', '')
-  var left = parseInt(rightNumbers, 10)
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
  
-  if (left > 0) {
+  if (right > 0) {
     dodger.style.right = `${right - 1}px`
   }
 }
