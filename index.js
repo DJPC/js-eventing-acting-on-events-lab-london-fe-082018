1b44cc0d3131fd882a24efeedd4cd8a688daedda
Donna Cadiente <djanecad@hotmail.com>
2018-10-10 17:42:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 var dodger = document.getElementById('dodger')
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.right.replace('px', '')
+  var left = parseInt(rightNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
